8a5c1ef7d20bd164acecb02e0512f9ff43c45b18
Leo <leo@Leos-MacBook-Pro.local>
2018-08-14 19:42:26 -0500

    My firts example on OpenGL

--- Example/main.cpp
+++ Example/main.cpp
@@ -1,15 +1,52 @@
-//
-//  main.cpp
-//  Example
-//
-//  Created by Leo on 8/14/18.
-//  Copyright © 2018 Leo. All rights reserved.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+//GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+//include
+#include <GLFW/glfw3.h>
+
+const GLint WIDTH= 800, HEIGHT=600;
+
+int main(){
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);//ONLY FOR MAC
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    
+    GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Example", nullptr, nullptr);
+    
+    int screenWidth, screenHeight;
+    glfwGetFramebufferSize(window, &screenWidth, &screenHeight);
+    
+    if(nullptr == window){
+        std::cout <<"Failed to create GLFW window\n a";
+        glfwTerminate();
+        
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glewExperimental = GL_TRUE;
+    
+    if(GLEW_OK != glewInit()){
+        std::cout << "Failed to initialized GLEW\n";
+        
+        return -1;
+    }
+    
+    glViewport(0, 0, screenWidth, screenHeight);
+    
+    while(!glfwWindowShouldClose(window)){
+        glfwPollEvents();
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        glfwSwapBuffers(window);
+    }
+    glfwTerminate();
+    
     return 0;
 }
